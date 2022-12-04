fc9e5e7e7a7e1d5e1ff220aed64282505a529921
AndreaIncatasciato <andrea.incatasciato93@gmail.com>
2022-12-04 13:00:53 +0000

    update fz on cross sections and energy levels

--- H2p.py
+++ H2p.py
@@ -10,6 +10,139 @@ from LWphotorates.utils import nu2lambda, lambda2nu
 from LWphotorates.utils import convert_energy_cm2ev, convert_energy_ev2cm, convert_energy_cm2k
 
 
+def get_ground_states_data():
+
+    '''
+    Get the H2p rotovibrational levels UGAMOP data of the electronic ground state.
+    Output:
+        ground_states_data: dictionary with:
+            'v': vibrational quantum number
+            'J': rotational quantum number
+            'eV': level energy in [eV]
+            'cm': level energy in [1/cm]
+            'K': equivalent temperature wrt the (0, 0) level (useful for the LTE partition function)
+    '''
+
+    energy_atomic_units = 2. * (const.Ryd) * (const.c).to(u.m * u.Hz) * (const.h).to(u.eV / u.Hz)
+
+    ground_states_path = os.path.join(os.path.dirname(__file__), 'inputdata', 'H2p', 'Xen.txt')
+    ground_states_file = np.loadtxt(ground_states_path, unpack=True)
+    ground_states_data = {
+        'v': np.array(ground_states_file[0], dtype=np.int16),
+        'J': np.array(ground_states_file[1], dtype=np.int16),
+        'eV': ground_states_file[2] * energy_atomic_units
+    }
+    ground_states_data['cm'] = convert_energy_ev2cm(ground_states_data['eV'])
+    ground_states_data['K'] = convert_energy_cm2k(ground_states_data['cm'][0] - ground_states_data['cm'])
+    
+    return ground_states_data
+
+
+def get_cross_section_zammit():
+
+    '''
+    Get the H2p (rotovibrational level resolved) cross sections from Zammit et al. (2017),
+    together with the kinetic energy released in the gas.
+    Output:
+        ground_states_data: dictionary with:
+
+            'v': vibrational quantum number
+            'J': rotational quantum number
+            'eV': level energy in [eV]
+            'cm': level energy in [1/cm]
+            'K': equivalent temperature wrt the (0, 0) level (useful for the LTE partition function)
+
+            'photon_energy': photon energy in [eV]
+            'photon_wl': photon wavelength in [A]
+            'cross_section': photodissociation cross section in [cm^2]
+            'heating_cross_section': heating cross section in [eV*cm^2]
+    '''
+
+    ground_states_data = get_ground_states_data()
+
+    cross_section_path = os.path.join(os.path.dirname(__file__), 'inputdata', 'H2p', 'Zammit2017.hdf5')
+
+    with h5py.File(name=cross_section_path, mode='r') as cross_section_file:
+        ground_states_data['photon_wl'] = cross_section_file['phot_wl'][:] * u.Angstrom
+        ground_states_data['photon_energy'] = cross_section_file['phot_energy'][:] * u.eV
+        ground_states_data['cross_section'] = cross_section_file['sigma'][:] * u.cm**2
+        ground_states_data['heating_cross_section'] = cross_section_file['sigma*en'][:] * u.eV * u.cm**2
+
+    return ground_states_data
+
+
+def get_cross_section_babb(wavelength_array):
+
+    '''
+    Get the H2p (rotovibrational level resolved) cross sections from Babb (2015),
+    together with the kinetic energy released in the gas.
+    NB: they included only a subset of all the rovib levels of the electronic ground state,
+    so here we are including only data relative to those levels.
+    Output:
+        ground_states_data: dictionary with:
+
+            'v': vibrational quantum number
+            'J': rotational quantum number
+            'eV': level energy in [eV]
+            'cm': level energy in [1/cm]
+            'K': equivalent temperature wrt the (0, 0) level (useful for the LTE partition function)
+
+            'photon_energy': photon energy in [eV]
+            'photon_wl': photon wavelength in [A]
+            'cross_section': photodissociation cross section in [cm^2]
+            'heating_cross_section': heating cross section in [eV*cm^2]
+    '''
+
+    energy_atomic_units = 2. * (const.Ryd) * (const.c).to(u.m * u.Hz) * (const.h).to(u.eV / u.Hz)
+
+    ground_states_data = get_ground_states_data()
+
+    cross_section_path = os.path.join(os.path.dirname(__file__), 'inputdata', 'H2p', 'Babb2015.txt')
+    cross_section_file = np.genfromtxt(cross_section_path, dtype={
+            'names': ('v', 'J', 'kinetic_energy', 'rovib_level_energy', 'photon_wl', 'Msquared'),
+            'formats':('i4', 'i4', 'f8', 'f8', 'f8', 'f8')}, unpack=False)
+
+    ground_states_data['photon_wl'] = wavelength_array
+    ground_states_data['photon_energy'] = lambda2nu(wavelength_array)[::-1] * (const.h).to(u.eV / u.Hz)
+    ground_states_data['cross_section'] = np.zeros(shape=(0, len(wavelength_array)))
+    ground_states_data['heating_cross_section'] = np.zeros(shape=(0, len(wavelength_array)))
+
+    # Babb dataset contains data only for a subset of the rotovib states of the electronic ground state (337 out of 423)
+    # the strategy here is to eliminate the states for which we don't have data
+    index_levels_no_data = []
+    for i in range(len(ground_states_data['eV'])):
+        vibr_quantum_number = ground_states_data['v'][i]
+        rot_quantum_number = ground_states_data['J'][i]
+        mask = (cross_section_file['v'] == vibr_quantum_number) & (cross_section_file['J'] == rot_quantum_number)
+        
+        if mask.sum():
+            original_wl = ((cross_section_file['photon_wl'][mask]) * u.nm).to(u.Angstrom)
+            original_cs = 2.689e-18 * u.cm**2 * cross_section_file['Msquared'][mask] * 45.563 / cross_section_file['photon_wl'][mask]
+            original_heating_cs = cross_section_file['kinetic_energy'][mask] * energy_atomic_units
+            
+            interp_cs = InterpolatedUnivariateSpline(x=original_wl, y=original_cs, k=1)
+            new_cs = interp_cs(ground_states_data['photon_wl'], ext='zeros')
+            interp_heating_cs = InterpolatedUnivariateSpline(x=original_wl, y=original_heating_cs, k=1)
+            new_heating_cs = interp_heating_cs(ground_states_data['photon_wl'], ext='zeros') * new_cs
+            
+            ground_states_data['cross_section'] = np.concatenate(
+                (ground_states_data['cross_section'], np.atleast_2d(new_cs)),
+                axis=0)
+            ground_states_data['heating_cross_section'] = np.concatenate(
+                (ground_states_data['heating_cross_section'], np.atleast_2d(new_heating_cs)),
+                axis=0)
+
+        else:
+            index_levels_no_data.append(i)
+
+    for key in ['v', 'J', 'eV', 'cm', 'K']:
+        ground_states_data[key] = np.delete(ground_states_data[key], index_levels_no_data)
+
+    ground_states_data['cross_section'] *= u.cm**2
+    ground_states_data['heating_cross_section'] *= (u.eV * u.cm**2)
+
+    return ground_states_data
+
 
 def read_Babb(wl_new):
     
