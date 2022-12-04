9a82ff04e44501945fc537116d0ec7b6d3ccf4a5
AndreaIncatasciato <andrea.incatasciato93@gmail.com>
2022-12-04 13:20:35 +0000

    first changes after function updates

--- H2p.py
+++ H2p.py
@@ -144,113 +144,6 @@ def get_cross_section_babb(wavelength_array):
     return ground_states_data
 
 
-def read_Babb(wl_new):
-    
-    '''
-    Read the H2p (rotovibrational level resolved) cross section from Babb (2015), together with the relative energies of the rovib levels
-    and the kinetic energy released in the gas.
-
-    Input:
-        high-resolution wavelength to resample the cross sections         [A]
-    Output:
-        Dictionary Xen:
-            rovib quantum numbers
-            energies of the levels                                        [1/cm]/[eV]/[K]
-        Dictionary sigma_pd:
-            cross sections                                                [cm^2]
-            kinetic energy released                                       [eV]
-    '''
-
-    Babb_f=os.path.dirname(os.path.abspath(__file__))+'/inputdata/H2p/Babb2015.txt'
-    Babb=np.loadtxt(Babb_f,unpack=True)
-
-    Babb_v=[]
-    Babb_j=[]
-    Babb_en=[]
-    Babb_sigma=[]
-    Babb_Ekin=[]
-
-    uniqv=np.unique(Babb[0])
-    for v in uniqv:
-        maskv=Babb[0]==v
-        uniqj=np.unique(Babb[1][maskv])
-        for j in uniqj:
-            maskj=Babb[1][maskv]==j
-            Babb_v.append(int(v))
-            Babb_j.append(int(j))
-            Babb_en.append(Babb[3][maskv][maskj][0])
-            Ekin=Babb[2][maskv][maskj]*u.rydberg.to(u.eV)*2*u.eV
-            wl=(Babb[4][maskv][maskj]*u.nm).to(u.angstrom)
-            sigma=2.689e-18*Babb[5][maskv][maskj]*45.563/Babb[4][maskv][maskj]*u.cm**2
-            interp=InterpolatedUnivariateSpline(x=wl,y=sigma,k=1)
-            sigma_new=interp(wl_new,ext='zeros')
-            Babb_sigma.append(sigma_new)
-            interp=InterpolatedUnivariateSpline(x=wl,y=Ekin,k=1)
-            Ekin_new=interp(wl_new,ext='zeros')
-            Babb_Ekin.append(Ekin_new)
-
-    Xen={
-        'v':np.array(Babb_v),
-        'J':np.array(Babb_j),
-        'cm':Babb_en/u.cm
-    }
-    Xen['eV']=convert_energy_cm2ev(Xen['cm'])
-    Xen['K']=convert_energy_cm2k(Xen['cm'][0]-Xen['cm'])
-
-    sigma_pd={
-        'sigma':np.array(Babb_sigma)*u.cm**2,
-        'heat_sigma':np.array(Babb_Ekin)*np.array(Babb_sigma)*u.cm**2*u.eV
-    }
-
-    return Xen,sigma_pd
-
-
-
-def read_Zammit(wl_new):
-    
-    '''
-    Read the UGAMOP data for the rovib states.
-    Read the H2p (rotovibrational level resolved) cross sections from Zammit et al. (2017), together with the kinetic energy released in the gas.
-
-    Input:
-        high-resolution wavelength to resample the cross sections         [A]
-    Output:
-        Dictionary Xen:
-            rovib quantum numbers
-            energies of the levels                                        [1/cm]/[eV]/[K]
-        Dictionary sigma_pd:
-            cross sections                                                [cm^2]
-            heating cross section                                         [eV]
-    '''
-
-    Xen=np.loadtxt(os.path.dirname(os.path.abspath(__file__))+'/inputdata/H2p/Xen.txt',unpack=True)
-    Xen={
-        'v':np.array(Xen[0],dtype=np.int16),
-        'J':np.array(Xen[1],dtype=np.int16),
-        'eV':Xen[2]*u.rydberg.to(u.eV)*2*u.eV
-    }
-    Xen['cm']=convert_energy_ev2cm(Xen['eV'])
-    Xen['K']=convert_energy_cm2k(Xen['cm'][0]-Xen['cm'])
-
-    Zammit_f=os.path.dirname(os.path.abspath(__file__))+'/inputdata/H2p/Zammit2017.hdf5'
-    f=h5py.File(name=Zammit_f,mode='r')
-
-    sigma=np.empty(shape=(len(f['sigma']),len(wl_new)))
-    heat_sigma=np.empty(shape=(len(f['sigma*en']),len(wl_new)))
-    for i in range(len(f['sigma'])):
-        interp=InterpolatedUnivariateSpline(x=f['phot_wl'],y=f['sigma'][i],k=1)
-        sigma[i]=interp(wl_new,ext='zeros')
-        interp=InterpolatedUnivariateSpline(x=f['phot_wl'],y=f['sigma*en'][i],k=1)
-        heat_sigma[i]=interp(wl_new,ext='zeros')
-
-    sigma_pd={
-        'sigma':sigma*u.cm**2,
-        'heat_sigma':heat_sigma*u.eV*u.cm**2
-    }
-
-    return Xen,sigma_pd
-
-
 def Xpop(Xen,Tgas):
 
     '''
@@ -299,7 +192,13 @@ def calc_ncrit(Tgas):
     return ((XH+Xe)*(XH/ncrH+Xe/ncre)**(-1))
 
 
-def calc_sigma(ngas,Tgas,wl_new,Dunn,Zammit):
+def calc_sigma(
+    ngas,
+    Tgas,
+    wl_new,
+    Dunn,
+    cross_section_reference='Z_17'
+    ):
 
     '''
     Given gas density and temperature, calculate the effective cross section to compute the dissociation rate and the heating rate.
@@ -315,10 +214,10 @@ def calc_sigma(ngas,Tgas,wl_new,Dunn,Zammit):
         heat: effective 'heating' cross section, same size as wavelength   [cm^2 eV]
     '''
 
-    if Zammit:
-        Xen,sigma_pd=read_Zammit(wl_new=wl_new)
-    else:
-        Xen,sigma_pd=read_Babb(wl_new=wl_new)
+    if cross_section_reference == 'Z_17':
+        ground_states_data = get_cross_section_zammit()
+    elif cross_section_reference == 'B_15':
+        ground_states_data = get_cross_section_babb(wavelength_array=wl_new)
 
     if Dunn:
         FCpop=np.array([0.08964,0.16013,0.17616,0.15592,0.12281,0.09052,0.06423,0.04465,0.03074,0.02111,0.01451,0.01002,0.00694,0.00480,0.00329,0.00221,0.00139,0.00072,0.00018])
@@ -339,9 +238,19 @@ def calc_sigma(ngas,Tgas,wl_new,Dunn,Zammit):
         return sigma,heat
 
 
-def calc_kH2p(lambda_array,spectra_lambda,distance,ngas,Tgas,Dunn=False,Zammit=True,
-    return_sigma=False,return_heating=False,
-    return_sigma_only=False,return_heating_only=False):
+def calc_kH2p(
+    lambda_array,
+    spectra_lambda,
+    distance,
+    ngas,
+    Tgas,
+    Dunn=False,
+    cross_section_reference='Z_17',
+    return_sigma=False,
+    return_heating=False,
+    return_sigma_only=False,
+    return_heating_only=False
+    ):
 
     '''
     Calculate the photodissociation rate of H2p its associated heating rate.
@@ -374,7 +283,13 @@ def calc_kH2p(lambda_array,spectra_lambda,distance,ngas,Tgas,Dunn=False,Zammit=T
     wl_new=(np.logspace(start=np.log10(min_wl.value),stop=np.log10(max_wl.value),num=int(1e5))*u.angstrom)
     en_new=lambda2nu(wl_new)*const.h.to(u.eV/u.Hz)
 
-    sigma,heating=calc_sigma(ngas=ngas,Tgas=Tgas,wl_new=wl_new,Dunn=Dunn,Zammit=Zammit)
+    sigma, heating = calc_sigma(
+        ngas=ngas,
+        Tgas=Tgas,
+        wl_new=wl_new,
+        Dunn=Dunn,
+        cross_section_reference=cross_section_reference
+        )
 
     if return_sigma_only:
         return sigma,wl_new
