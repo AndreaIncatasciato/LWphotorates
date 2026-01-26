289c99f160c0b01ebcb180e10a61b37cea687615
Andrea Incatasciato <andrea.incatasciato93@gmail.com>
2026-01-26 17:34:36 +0000

    add wishart cross section

--- LWphotorates/HM.py
+++ LWphotorates/HM.py
@@ -10,8 +10,9 @@ def get_cross_section(reference='ML_17'):
 
     '''
     Get the cross section as a function of the photon energy.
-    Four possible references:
+    Five possible references:
       - ML_17: McLaughlin et al. (2017) https://ui.adsabs.harvard.edu/abs/2017JPhB...50k4001M/abstract
+      - W_79: Wishart (1979) https://ui.adsabs.harvard.edu/abs/1979MNRAS.187P..59W/abstract
       - SK_87: Shapiro & Kang (1987) https://ui.adsabs.harvard.edu/abs/1987ApJ...318...32S/abstract
       - J_88: John (1988) https://ui.adsabs.harvard.edu/abs/1988A%26A...193..189J/abstract
       - C_07: Chuzhoy et al. (2007) https://ui.adsabs.harvard.edu/abs/2007ApJ...665L..85C/abstract
@@ -44,6 +45,23 @@ def get_cross_section(reference='ML_17'):
             'frequency': frequency_array[mask],
             'wavelength': wavelength_array[mask],
             'cross_section': cross_section_array[mask]
+        }
+
+    elif reference == 'W_79':
+
+        input_file_path = os.path.join(os.path.dirname(__file__), 'inputdata', 'HM', 'sigma_Wishart1979.txt')
+
+        wavelength_array, cross_section_array = np.loadtxt(input_file_path, unpack=True)
+        wavelength_array = wavelength_array * u.AA
+        cross_section_array = cross_section_array * 1e-18 * u.cm**2
+        frequency_array = lambda2nu(wavelength_array)
+        energy_array = const.h.to(u.eV / u.Hz) * frequency_array
+
+        data_dictionary = {
+            'energy': energy_array,
+            'frequency': frequency_array,
+            'wavelength': wavelength_array,
+            'cross_section': cross_section_array
         }
 
     elif reference == 'SK_87':
